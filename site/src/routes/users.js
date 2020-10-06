47c5c37a16379c93c38f81df8da8f13cbed3c4b5
agusmfranco <agustinmfranco@hotmail.com>
2020-10-06 15:42:16 -0300

    Nuevo proyecto

---
+++ site/src/routes/users.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+let multer = require('multer');
+let path = require('path');
+
+/* Multer */
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'tmp/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+});
+ 
+var upload = multer({ storage: storage });
+/* Multer */
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
