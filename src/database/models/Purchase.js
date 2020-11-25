dcb203f46c6ba1f40afa5eab6303ccf7d3d1afdf
agusmfranco <agustinmfranco@hotmail.com>
2020-11-25 16:39:53 -0300

    sq aplicado

---
+++ src/database/models/Purchase.js
@@ -0,0 +1,37 @@
+const {sequelize, dataTypes} = require('sequelize');
+
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Purchase';
+    let cols = {
+        total: {
+            type: dataTypes.INTEGER
+        },
+        user_id: {
+            type: dataTypes.INTEGER
+        },
+        product_id: {
+            type: dataTypes.INTEGER
+        }
+    };
+    let config = {
+        tablename: 'purchase',
+        timestamps: false
+    };
+    const Purchase = sequelize.define(alias, cols, config);
+
+    Purchase.associate = function(models){
+        Purchase.hasMany(models.Users, {
+            as: 'users',
+            foreignKey: 'purchase_id'
+        });
+        Purchase.belongsToMany(models.Books, {
+            as: 'books',
+            through: 'books_purchase',
+            foreignKey: 'book_id',
+            otherKey: 'purchase_id',
+            timestamps: false
+        });
+    };
+
+    return Purchase;
+}
---
+++ src/database/models/User.js
@@ -0,0 +1,49 @@
+const {sequelize, dataTypes} = require('sequelize');
+
+
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Users';
+    let cols = {
+        email: {
+            type: dataTypes.STRING,
+            allownull: false
+        },
+        name: {
+            type: dataTypes.STRING,
+            allownull: false
+        },
+        dni: {
+            type: dataTypes.INTEGER,
+            allownull: false
+        },
+        adress: {
+            type: dataTypes.STRING,
+            allownull: false
+        },
+        password: {
+            type: dataTypes.STRING,
+            allownull: false
+        },
+        birth: {
+            type: dataTypes.DATEONLY,
+            allownull: false
+        },
+        purchase_id: {
+            type: dataTypes.INTEGER
+        } 
+    };
+    let config = {
+        tablename: 'users',
+        timestamps: false
+    };
+    const Users = sequelize.define(alias, cols, config);
+
+    Users.associate = function (models) {
+        Users.belongsTo(models.Purchase, {
+          as: 'purchase',
+          foreignKey: 'purchase_id',
+        });
+    };
+
+    return Users;
+}
