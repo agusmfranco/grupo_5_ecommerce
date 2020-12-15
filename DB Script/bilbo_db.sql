117dc68fab4fe45fdd2f43ae071006adbc0ef19b
hernanponcetta <hernanponcetta@gmail.com>
2020-12-15 19:51:16 -0300

    Base de datos actualizada

--- DB Script/bilbo_db.sql
+++ DB Script/bilbo_db.sql
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -108,6 +108,37 @@ CREATE TABLE `books_purchase` (
 LOCK TABLES `books_purchase` WRITE;
 /*!40000 ALTER TABLE `books_purchase` DISABLE KEYS */;
 /*!40000 ALTER TABLE `books_purchase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checkouts`
+--
+
+DROP TABLE IF EXISTS `checkouts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checkouts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `book_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `state` int DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `book_id` (`book_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `checkouts_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `checkouts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checkouts`
+--
+
+LOCK TABLES `checkouts` WRITE;
+/*!40000 ALTER TABLE `checkouts` DISABLE KEYS */;
+INSERT INTO `checkouts` VALUES (36,4,6,1,1),(37,7,6,4,1);
+/*!40000 ALTER TABLE `checkouts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -268,6 +299,10 @@ LOCK TABLES `userstypes` WRITE;
 INSERT INTO `userstypes` VALUES (1,'administrador'),(2,'cliente');
 /*!40000 ALTER TABLE `userstypes` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'biblo_db'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -278,4 +313,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-03 16:29:40
+-- Dump completed on 2020-12-15 19:49:21
