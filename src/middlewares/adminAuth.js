8c1a42f24923dce02fcd02a69e45ee172e679626
hernanponcetta <hernanponcetta@gmail.com>
2020-12-21 19:57:23 -0300

    Validaciones actualizadas

--- src/middlewares/adminAuth.js
+++ src/middlewares/adminAuth.js
@@ -1,11 +1,10 @@
 const session = require("express-session");
 const db = require("../database/models");
 
-
-const adminAuth = function(req, res, next){
-    if (req.session.loggedUser.user_type_id == 1){
-        next();
-    }
+const adminAuth = function (req, res, next) {
+  if (req.session.loggedUser.user_type_id == 1) {
+    next();
+  }
 };
 
 module.exports = adminAuth;
